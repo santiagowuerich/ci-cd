e750ac52fb8be8001eecac41c9be3da7615b562a
Santiago Nicolas Wuerich <santiagow142001@gmail.com>
2026-05-27 19:58:09 -0300

    rompemos

--- test/test.js
+++ test/test.js
@@ -12,7 +12,7 @@ describe('Operaciones matemáticas', () => {
   });
 
   test('resta correctamente 10 - 4', () => {
-    expect(restar(10, 4)).toBe(6);
+    expect(restar(10, 4)).toBe(9);
   });
 
   test('resta con resultado negativo', () => {
